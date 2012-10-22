bfb0141a1544f8b94f8ec48bf1965655eab9647b
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2012-10-22 15:42:36 +0200

    Status quo

---
+++ scripts/java/ExampleScript.java
@@ -0,0 +1,8 @@
+public class ExampleScript {
+
+	// Run with: ant run-script -Dscript.class=ExampleScript
+	public static void main(String[] args) {
+		System.out.println("This is an example script...");
+	}
+
+}
